53b2e0331062f2c7e48fcd9bb7d5922c730d8d91
Srijita Mandal <84610348+Srijita-Mandal@users.noreply.github.com>
2021-11-28 00:57:52 +0530

    Add files via upload

---
+++ Zero_one_triangle.java
@@ -0,0 +1,27 @@
+//print pattern-> 0-1 triangle
+//1
+//0 1
+//1 0 1
+//0 1 0 1
+//1 0 1 0 1
+
+import java.util.*;
+
+public class Zero_one_triangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+            if((i+j)%2 == 0){
+                System.out.print("1 ");
+            } else{
+                System.out.print("0 ");
+            }
+          }
+          System.out.println();
+        }
+    }
+}
